90529c4d028e1c81d1ddc88d6ad0fed5c2d2691a
Yusuf <yakande96@gmail.com>
2024-02-14 00:04:48 +0100

    another minor update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 
 const corsOptions = {
   origin: "*",
-  // credentials: true, //access-control-allow-credentials:true
+  credentials: true, //access-control-allow-credentials:true
   // optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
