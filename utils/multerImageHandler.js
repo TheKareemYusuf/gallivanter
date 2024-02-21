b2530fc7eaf554d4898ca7bf096bbddf383342a3
Yusuf <yakande96@gmail.com>
2024-02-21 20:13:01 +0100

    testing image upload

--- utils/multerImageHandler.js
+++ utils/multerImageHandler.js
@@ -1,10 +1,37 @@
-const multer = require("multer");
-const AppError = require('./appError');
+// const multer = require("multer");
+// const AppError = require('./appError');
+
+// // const multerStorage = multer.memoryStorage();
 
 // const multerStorage = multer.memoryStorage();
 
-const multerStorage = multer.memoryStorage();
 
+// const multerFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith("image")) {
+//     cb(null, true);
+//   } else {
+//     cb(new AppError("Not an image! Please upload only images.", 400), false);
+//   }
+// }; 
+
+// const uploadPicture = multer({
+//   storage: multerStorage,
+//   fileFilter: multerFilter,
+// });
+
+// module.exports = uploadPicture;
+
+const multer = require("multer");
+const AppError = require('./appError');
+
+const multerStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads'); // Destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Keep original file name
+  }
+});
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
